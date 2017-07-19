4090573f64cc72729f6bad9bcd6de2ec700154e5
atf471 <fua2@bxscience.edu>
2017-07-19 09:46:40 -0400

    Added python folder

---
+++ python/draw_shapes.py
@@ -0,0 +1,48 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = 120
+y_pos = 0
+t.setposition(x_pos, y_pos)
+
+color1= input("What color?")
+for c in range():
+	t.fillcolor()
+	t.begin_fill(color2)
+	
+sides = int(input("How many sides?"))
+for b in range(sides):
+	t.forward(80)
+	t.left(360/sides)
+
+t.end_fill(BlanchedAlmond)	
+
+# person = input('How many sides?')
+# for x in range(person)
+# 	forward(80)
+# 	left(360/person)
+	
+# for b in range(3):
+# 	forward(120)
+# 	left(120)
+
+# right(90)
+# forward(120)
+# left(90)
+# forward(120)
+# goto(120,0)
+
+### Write your code below:
+
+
+
+
+
+
+# Close window on click.
+exitonclick()
